747b18308d692101a0d4db22614f1eb405c1b098
Amir Rauf <arauf@carcrave.net>
2020-08-17 17:20:18 -0500

    Initial Checkin

---
+++ mssc-scm/src/main/java/guru/springframework/msscscm/MsscScmApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscscm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscScmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscScmApplication.class, args);
+	}
+
+}
---
+++ mssc-scm/src/test/java/guru/springframework/msscscm/MsscScmApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscscm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscScmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
